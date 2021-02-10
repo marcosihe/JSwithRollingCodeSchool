f15a236039d15106f5b5295c8c6c9b8115fe52a9
Marcos Herrera <marcosherrera.ib@gmail.com>
2021-02-10 12:06:28 -0300

    se trabajo sobre la validacion de los campos email y numero de telefono dentro del formulario

--- 06-eventos/index.js
+++ 06-eventos/index.js
@@ -1,9 +1,28 @@
-requiredField = () => {
+requiredField = (input) => {
     console.log('Se cambió el foco');
-    let input = document.getElementById('name');
-    if (input.value.length == 0) {
+    //let input = document.getElementById('name');
+    if (input.value.trim() === '') { //input.value.lenght === '' //Es otra opción compara con la cadena vacía
         input.className = 'form-control is-invalid';
     }else{
         input.className = 'form-control is-valid';
+    }
+}
+
+//marcos@gmail.com
+validateEmail = (email) => {
+    let expression = /\w+@\w+\.[a-z]{2,}$/;
+    if(email.value.trim() != '' && expression.test(email.value)){
+        email.className = 'form-control is-valid';
+    }else {
+        email.className = 'form-control is-invalid';
+    }
+}
+
+//validar número de teléfono
+validateNumber = (number) => {
+    if (number.value.trim() != '' && !isNaN(number.value)) {
+        number.className = 'form-control is-valid';
+    }else{
+        number.className = 'form-control is-invalid';
     }
 }
