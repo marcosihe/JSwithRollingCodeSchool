0297a18a63d55311028db6a1bdad26bb43f6cf1b
Marcos Herrera <marcosherrera.ib@gmail.com>
2021-02-26 00:56:17 -0300

    Se terminó la parte de las validaciones y se agregó el envío de los datos cargados en formulario a mi e-mail mediante 'emailjs.com'

--- 06-eventos/index.js
+++ 06-eventos/index.js
@@ -1,10 +1,10 @@
 requiredField = (input) => {
-    console.log('Se cambió el foco');
-    //let input = document.getElementById('name');
-    if (input.value.trim() === '') { //input.value.lenght === '' //Es otra opción compara con la cadena vacía
+    if (input.value.trim() === '') { //input.value.length === '' //Es otra opción compara con la cadena vacía
         input.className = 'form-control is-invalid';
+        return false;
     }else{
         input.className = 'form-control is-valid';
+        return true;
     }
 }
 
@@ -13,8 +13,10 @@ validateEmail = (email) => {
     let expression = /\w+@\w+\.[a-z]{2,}$/;
     if(email.value.trim() != '' && expression.test(email.value)){
         email.className = 'form-control is-valid';
+        return true;
     }else {
         email.className = 'form-control is-invalid';
+        return false;
     }
 }
 
@@ -22,8 +24,10 @@ validateEmail = (email) => {
 validateNumber = (number) => {
     if (number.value.trim() != '' && !isNaN(number.value)) {
         number.className = 'form-control is-valid';
+        return true;
     }else{
         number.className = 'form-control is-invalid';
+        return false;
     }
 }
 
@@ -31,7 +35,56 @@ validateNumber = (number) => {
 validateConsult = text => {
     if (text.value.trim() != '' && text.value.length >= 10) {
         text.className = 'form-control is-valid';
+        return true;
     }else{
         text.className = 'form-control is-invalid';
+        return false;
+    }
+}
+
+//validar check-términos y condiciones
+validateTerms = () => {
+    if (checkTerms.checked) {
+        checkTerms.className = 'form-check-input is-valid';
+        return true;
+    }else{
+        checkTerms.className = 'form-check-input is-invalid';
+        return false;
+    }
+}
+let checkTerms = document.querySelector('#terms');
+checkTerms.addEventListener('change', validateTerms);
+
+//Validación general - submit button
+let sendEmail = () => {
+    emailjs.send("service_m4vl49a","template_tny4kwn",{
+        from_name: document.querySelector('#name').value,
+        to_name: "Administrador",
+        consult: document.querySelector('#consult').value,
+        email: document.querySelector('#email').value,
+        phone: document.querySelector('#phone').value 
+        }).then(function (response){
+            //Esta función se ejecuta cuando se cumplió la promesa
+            console.log(response);
+            alert('Mensaje enviado');
+        }, function (error){
+            console.log(error);
+            alert('Ocurrió un error. Intente más tarde.');
+        })
+}
+
+generalValidation = e => { //el parámetro e hace referencia al 'event' 
+    e.preventDefault();    //en html se usa literalmente la palabra 'event' pues hace referencia al objeto de ese nombre
+    //console.log(e);
+    if (requiredField(document.querySelector('#name')) &&
+    validateEmail(document.querySelector('#email')) &&
+    validateNumber(document.querySelector('#phone')) &&
+    validateConsult(document.querySelector('#consult')) &&
+    validateTerms()) {
+        //En este punto se debe envia el mail
+        //alert('Datos correctos');
+        sendEmail();
+    }else{
+        //alert('Datos incorrectos');
     }
 }
