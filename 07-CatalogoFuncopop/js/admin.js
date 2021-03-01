e68e5bf723830be1cb8dc95f1c1d06cf28f692ed
Marcos Herrera <marcosherrera.ib@gmail.com>
2021-03-01 08:51:28 -0300

    Se agrego el maquetado la página index y pagina admin

---
+++ 07-CatalogoFuncopop/js/admin.js
@@ -0,0 +1,12 @@
+class Funko{
+    constructor(codigo, nombre, numSerie, categoria, descripcion, imagen){
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.numSerie = numSerie;
+        this.categoria = categoria;
+        this.descripcion = descripcion;
+        this.imagen = imagen;
+    }
+}
+
+let nuevoFunko = new Funko();
